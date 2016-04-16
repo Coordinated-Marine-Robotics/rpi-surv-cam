b08ea14290a66ab28de30f42507e159c3ed668ab
Amit Oren <aoren@infinidat.com>
2016-04-16 14:07:37 +0300

    Adding djcelery instead

--- rpisurvcam/rpisurvcam/settings.py
+++ rpisurvcam/rpisurvcam/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
+# For django-celery:
+import djcelery
+djcelery.setup_loader()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -41,10 +45,8 @@ INSTALLED_APPS = [
     'gunicorn',
     'survcam',
     'events',
+    'djcelery',
 ]
-
-SOCKETIO_HOST = '127.0.0.1'
-SOCKETIO_PORT = 9999
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
