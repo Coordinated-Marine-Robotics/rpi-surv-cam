289612ef3e1493b6bb6c889f7e7e7092b615e00a
maavi <lbgp.avi@gmail.com>
2016-05-18 20:00:53 +0000

    Camera model and representation changes: 1. Not showing camera name and location under Camera details when no camera object in DB. 2. Removed stream_url, commands_queue, resolution and fps fields from model. 3. Added validation error that occurs when trying to create more than one camera object.

--- rpisurvcam/survcam/models.py
+++ rpisurvcam/survcam/models.py
@@ -1,23 +1,15 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
-# Create your models here.
+from django.core.exceptions import ValidationError
 
 class Camera(models.Model):
-    RESOLUTION_CHOICES = (
-        ("640x480", "640x480"),
-        ("1296x730", "1296x730"),
-        ("1296x972", "1296x972"),
-        ("1024x768", "1024x768"),
-        ("2592x1944", "2592x1944"),
-        ("1920x1080", "1920x1080")
-    )
-
     name = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
-    stream_url = models.URLField()
-    commands_queue = models.CharField(max_length=100)
-    resolution = models.CharField(choices=RESOLUTION_CHOICES,
-                                  max_length=100, default='1024x768')
-    fps = models.IntegerField(default=30)
+
+    def clean(self):
+        model = self.__class__
+        if (model.objects.count() > 0 and self.id != model.objects.get().id):
+            raise ValidationError("You can only create one Camera instance. \
+            Please remove the first instance, \
+            or change it's values instead of creating a new Camera.")
